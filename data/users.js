92ad41c5438ddbaacdf557decbfade9169eca48e
OficialTheFoxiYT <wolfstudio.office@gmail.com>
2025-06-30 14:58:17 -0300

    Update and rename users.json to users.js

---
+++ data/users.js
@@ -0,0 +1,7 @@
+const users = [
+  { username: "owner", password: "1234", role: "owner" },
+  { username: "subowner", password: "sub123", role: "subowner" },
+  { username: "admin", password: "admin123", role: "admin" },
+  { username: "mod1", password: "mod123", role: "mod" },
+  { username: "helper1", password: "help123", role: "helper" }
+];
